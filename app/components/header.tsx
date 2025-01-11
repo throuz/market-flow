50812d483e35f044164f2bd62b3855ca16e424bf
Throuz <zed2786328@gmail.com>
2025-01-11 15:51:15 +0800

    optimize header

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -1,21 +1,15 @@
-import DeployButton from "@/components/deploy-button";
-import { EnvVarWarning } from "@/components/env-var-warning";
 import HeaderAuth from "@/components/header-auth";
-import { hasEnvVars } from "@/utils/supabase/check-env-vars";
 import Link from "next/link";
 
 export default function Header() {
   return (
     <header>
       <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
-        <div className="w-full max-w-5xl flex justify-between items-center p-3 px-5 text-sm">
+        <div className="w-full flex justify-between items-center p-3 px-5 text-sm">
           <div className="flex gap-5 items-center font-semibold">
-            <Link href={"/"}>Next.js Supabase Starter</Link>
-            <div className="flex items-center gap-2">
-              <DeployButton />
-            </div>
+            <Link href="/">Market flow</Link>
           </div>
-          {!hasEnvVars ? <EnvVarWarning /> : <HeaderAuth />}
+          <HeaderAuth />
         </div>
       </nav>
     </header>
