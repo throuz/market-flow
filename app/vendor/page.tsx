5ad4660b415d33dd9d58a0d58f71cc5e68968ad2
Throuz <zed2786328@gmail.com>
2025-02-03 17:19:23 +0800

    optimize vendor page

--- app/vendor/page.tsx
+++ app/vendor/page.tsx
@@ -1,17 +1,23 @@
 import { createClient } from "@/utils/supabase/server";
-import { InfoIcon } from "lucide-react";
+import { redirect } from "next/navigation";
 
 export default async function VendorPage() {
   const supabase = await createClient();
 
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+
+  if (!user) {
+    return redirect("/sign-in");
+  }
+
   return (
-    <div className="flex-1 w-full flex flex-col gap-12">
-      <div className="w-full">
-        <div className="bg-accent text-sm p-3 px-5 rounded-md text-foreground flex gap-3 items-center">
-          <InfoIcon size="16" strokeWidth={2} />
-          This is vendor page
-        </div>
-      </div>
+    <div className="flex flex-col gap-2 items-center p-10">
+      <h2 className="font-bold text-2xl mb-4">Your user details</h2>
+      <pre className="text-xs font-mono p-3 rounded border max-h-320 overflow-auto">
+        {JSON.stringify(user, null, 2)}
+      </pre>
     </div>
   );
 }
