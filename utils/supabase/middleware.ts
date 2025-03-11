a8a43839916bca072c48f0a92b8fe402b0ffb631
Throuz <zed2786328@gmail.com>
2025-03-11 09:42:57 +0800

    optimize middelware

--- utils/supabase/middleware.ts
+++ utils/supabase/middleware.ts
@@ -3,9 +3,7 @@ import { createServerClient } from "@supabase/ssr";
 import { type NextRequest, NextResponse } from "next/server";
 
 export const updateSession = async (request: NextRequest) => {
-  const response = NextResponse.next({
-    request: { headers: request.headers },
-  });
+  const response = NextResponse.next({ request: { headers: request.headers } });
 
   const supabase = createServerClient<Database>(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
@@ -13,11 +11,10 @@ export const updateSession = async (request: NextRequest) => {
     {
       cookies: {
         getAll: () => request.cookies.getAll(),
-        setAll: (cookiesToSet) => {
+        setAll: (cookiesToSet) =>
           cookiesToSet.forEach(({ name, value, options }) =>
             response.cookies.set(name, value, options)
-          );
-        },
+          ),
       },
     }
   );
@@ -33,30 +30,31 @@ export const updateSession = async (request: NextRequest) => {
     pathname.startsWith(route)
   );
 
+  // Redirect to sign-in if not logged in and the route is protected
   if (!isLoggedIn && isProtected) {
     return NextResponse.redirect(new URL("/sign-in", request.url));
   }
 
-  if (isLoggedIn && pathname === "/") {
-    const { data: profile } = await supabase
-      .from("profiles")
-      .select("role")
-      .eq("id", user?.id ?? "")
-      .single();
+  const { data: profile } = await supabase
+    .from("profiles")
+    .select("role")
+    .eq("id", user?.id ?? "")
+    .single();
 
-    const roleRedirects: Record<
-      Database["public"]["Enums"]["app_role"],
-      string
-    > = {
-      admin: "/admin",
-      vendor: "/vendor",
-      customer: "/customer",
-    };
+  const rolePaths: Record<Database["public"]["Enums"]["app_role"], string> = {
+    admin: "/admin",
+    vendor: "/vendor",
+    customer: "/customer",
+  };
 
-    const redirectUrl = profile?.role
-      ? roleRedirects[profile.role]
-      : "/protected";
-    return NextResponse.redirect(new URL(redirectUrl, request.url));
+  // Redirect users to their respective role-based pages
+  if (
+    isLoggedIn &&
+    profile?.role &&
+    !pathname.startsWith("/protected") &&
+    !pathname.startsWith(rolePaths[profile.role])
+  ) {
+    return NextResponse.redirect(new URL(rolePaths[profile.role], request.url));
   }
 
   return response;
