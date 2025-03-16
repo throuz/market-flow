dc3df7e6593a0c1da6e06b7a9eef7aaeac0aa3fd
Throuz <zed2786328@gmail.com>
2025-03-16 09:13:32 +0800

    OrderStatusBadge optimization

--- components/OrderStatusBadge.tsx
+++ components/OrderStatusBadge.tsx
@@ -2,22 +2,16 @@
 
 import { Database } from "@/database.types";
 import useOrderStatus from "@/hooks/useOrderStatus";
+import { Badge } from "@/components/ui/badge";
 
-const getStatusStyle = (
-  status: Database["public"]["Enums"]["order_status"]
-) => {
-  switch (status) {
-    case "pending":
-      return "bg-yellow-200 text-yellow-800";
-    case "processing":
-      return "bg-blue-200 text-blue-800";
-    case "completed":
-      return "bg-green-200 text-green-800";
-    case "cancelled":
-      return "bg-red-200 text-red-800";
-    default:
-      return "bg-gray-200 text-gray-800";
-  }
+const statusVariants: Record<
+  Database["public"]["Enums"]["order_status"],
+  string
+> = {
+  pending: "bg-yellow-200 text-yellow-800",
+  processing: "bg-blue-200 text-blue-800",
+  completed: "bg-green-200 text-green-800",
+  cancelled: "bg-red-200 text-red-800",
 };
 
 interface OrderStatusBadgeProps {
@@ -26,13 +20,13 @@ interface OrderStatusBadgeProps {
 
 export default function OrderStatusBadge({ status }: OrderStatusBadgeProps) {
   const { orderStatusOptions } = useOrderStatus();
+  const label =
+    orderStatusOptions.find((option) => option.value === status)?.label ??
+    "N/A";
 
   return (
-    <span
-      className={`px-2 py-1 rounded-full text-sm ${getStatusStyle(status)}`}
-    >
-      {orderStatusOptions.find((option) => option.value === status)?.label ??
-        "N/A"}
-    </span>
+    <Badge className={statusVariants[status] || "bg-gray-200 text-gray-800"}>
+      {label}
+    </Badge>
   );
 }
