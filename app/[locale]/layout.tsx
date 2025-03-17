95232bffe3eaeeb95282ae3d9f785ee77de2b8bd
Throuz <zed2786328@gmail.com>
2025-03-17 22:21:37 +0800

    change metadata

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -18,8 +18,8 @@ const defaultUrl = process.env.VERCEL_URL
 
 export const metadata = {
   metadataBase: new URL(defaultUrl),
-  title: "Next.js and Supabase Starter Kit",
-  description: "The fastest way to build apps with Next.js and Supabase",
+  title: "Market Flow",
+  description: "Find the freshest vegetables and fruits for your kitchen",
 };
 
 const geistSans = Geist({
